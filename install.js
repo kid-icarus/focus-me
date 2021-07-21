964ad326b2b267ca8603278ab907351ef56aa097
Ryan Kois <ryan.kois@gmail.com>
2021-07-21 16:28:20 -0700

    Delete applescripts dir if already exists.

--- install.js
+++ install.js
@@ -30,8 +30,11 @@ const install = () => {
     })
   }
 
-  console.log('Symlinking compiled applescript libraries.')
-  fs.symlinkSync(join(__dirname, 'dist/util/focus-me-applescripts'), focusMeDir);
+  if (!fs.existsSync(focusMeDir)) {
+    fs.unlinkSync(focusMeDir)
+    console.log('Symlinking compiled applescript libraries.')
+    fs.symlinkSync(join(__dirname, 'dist/util/focus-me-applescripts'), focusMeDir);
+  }
 }
 
 install();
